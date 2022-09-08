1f52a1404ea7a28f9b7eb9519970031b054c7432
Francis sathekge <francissathekge@gmail.com>
2022-09-08 12:31:17 +0200

    first commit

---
+++ app/src/main/java/com/example/mynotes/NoteAdapter.java
@@ -0,0 +1,63 @@
+package com.example.mynotes;
+
+import android.content.Context;
+import android.content.Intent;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.helper.widget.Layer;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class NoteAdapter extends FirestoreRecyclerAdapter<Note, NoteAdapter.NoteViewHandler> {
+    Context context;
+
+    public NoteAdapter(@NonNull FirestoreRecyclerOptions<Note> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull NoteViewHandler holder, int position, @NonNull Note note) {
+        holder.titleTextview.setText(note.title);
+        holder.contentTextView.setText(note.content);
+        holder.timestampTextView.setText(Utility.timestampToString(note.timestamp));
+
+        holder.itemView.setOnClickListener((v)->{
+            Intent intent = new Intent(context,NotesDetailsActivity.class);
+            intent.putExtra("title",note.title);
+            intent.putExtra("content",note.content);
+            String docId = this.getSnapshots().getSnapshot(position).getId();
+            intent.putExtra("docId",docId);
+            context.startActivity(intent);
+
+        });
+
+
+    }
+
+    @NonNull
+    @Override
+    public NoteViewHandler onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_note_item,parent,false);
+        return new NoteViewHandler(view);
+    }
+
+    class NoteViewHandler extends RecyclerView.ViewHolder{
+
+        TextView titleTextview,contentTextView,timestampTextView;
+
+        public NoteViewHandler(@NonNull View itemView) {
+            super(itemView);
+            titleTextview = itemView.findViewById(R.id.note_title_text_view);
+            contentTextView = itemView.findViewById(R.id.note_content_text_view);
+            timestampTextView = itemView.findViewById(R.id.note_timestamp_text_view);
+        }
+    }
+}
